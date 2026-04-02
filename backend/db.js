6eff62ec210e7af47fcba2be222f8268e1128029
Rishabh Anand Singh <rishabhanandsingh277@gmail.com>
2026-04-02 22:05:41 +0530

    Added MongoDB connection

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/healthDB")
+
+        console.log("MongoDB Connected")
+
+    } catch (error) {
+        console.log("MongoDB Connection Failed")
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const cors = require("cors")
+const connectDB = require("./db")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+connectDB()
 
 app.get("/", (req, res) => {
     res.send("Health Aggregator Server Running")
